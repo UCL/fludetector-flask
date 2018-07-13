dca3257ff8ebcd918501e1a3c55f0281d7bbcb1e
David Guzman <d.guzman@ucl.ac.uk>
2018-07-13 17:13:49 +0100

    Move scheduling from cronjob to apscheduler

--- fludetector/scripts.py
+++ fludetector/scripts.py
@@ -7,6 +7,13 @@ from fludetector.sources import google, csv_, twitter
 from fludetector.errors import FluDetectorError
 from fludetector.models import db, Model
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+import logging
+
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 
 MODEL_TYPES = {
     'google': google,
@@ -62,3 +69,30 @@ def init_app(app):
     command(initdb)
     command(listmodels)
     command(runmodel)
+    command(runmodelscheduler)
+
+
+def runmodel_func(model_id):
+    from fludetector.app import app
+    with app.app_context():
+        try:
+            model = Model.query.filter_by(id=model_id).one()
+            start = model.last_score.day + timedelta(days=1)
+            end = date.today() - timedelta(days=2)
+            MODEL_TYPES[model.type].run(model, start, end, csv_file=None)
+        except NoResultFound:
+            raise click.ClickException('Could not find model with that ID')
+        except FluDetectorError as e:
+            raise click.ClickException(e.message)
+
+
+@click.argument('model_id', type=int)
+@click.argument('crontab', type=str)
+def runmodelscheduler(model_id, crontab):
+    scheduler = BlockingScheduler()
+    scheduler.add_job(
+        func=runmodel_func,
+        kwargs={"model_id": model_id},
+        trigger=CronTrigger.from_crontab(crontab))
+    scheduler.start()
+
--- scripts/run.sh
+++ scripts/run.sh
@@ -12,4 +12,4 @@ set -e
 
 export FLASK_APP=./fludetector/app.py
 
-flask runmodel $@
+flask "$@"
